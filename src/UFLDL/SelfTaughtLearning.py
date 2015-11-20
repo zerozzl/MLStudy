0900b9002f3ce4d128c91fdd500865908d289626
zerozzl <444056035@qq.com>
2015-11-20 13:59:44 +0800

    独立MNIST数据读取模块

--- src/UFLDL/SelfTaughtLearning.py
+++ src/UFLDL/SelfTaughtLearning.py
@@ -1,5 +1,11 @@
-'''
-Created on 2015Äê11ÔÂ19ÈÕ
+#coding: UTF-8
+import struct
+import numpy as np
 
-@author: Administrator
-'''
+def main():
+    inputSize  = 28 * 28;
+    numLabels  = 5;
+    hiddenSize = 200;
+    sparsityParam = 0.1;
+    lamb = 3e-3;
+    beta = 3;
