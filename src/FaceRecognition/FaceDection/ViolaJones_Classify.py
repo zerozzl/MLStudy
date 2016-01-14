efbec175b3fa0fbc27e7d813509a63f281fa2e5c
zerozzl <444056035@qq.com>
2016-01-14 17:11:35 +0800

    update viola jones classify

--- src/FaceRecognition/FaceDection/ViolaJones_Classify.py
+++ src/FaceRecognition/FaceDection/ViolaJones_Classify.py
@@ -1,5 +1,6 @@
 # -*- coding:utf-8 -*-
 from PIL import Image
+import ImageDraw
 import numpy as np
 import os
 import matplotlib.pyplot as plt
@@ -12,7 +13,7 @@ def readImage(filepath, imgSize=None):
         im = im.resize((imgSize, imgSize));
     
     im = np.array(im);
-    im = (im - np.mean(im)) / 255;
+#     im = (im - np.mean(im)) / 255;
     return im;
 
 # 画出源数据
@@ -63,94 +64,98 @@ class HaarLikeFeature:
         self.p = p;
         self.weight = weight;
     
-    def getEigenvalue(self, intImg):
+    def getEigenvalue(self, ftype, pos, w, h, intImg):
         eigenvalue = 0;
-        if self.type == '1a':
-            part = self.w / 2;
-            negative = intImg.getAreaSum(self.pos[1], self.pos[0], part, self.h);
-            positive = intImg.getAreaSum(self.pos[1] + part, self.pos[0], part, self.h);
+        if ftype == '1a':
+            part = w / 2;
+            negative = intImg.getAreaSum(pos[1], pos[0], part, h);
+            positive = intImg.getAreaSum(pos[1] + part, pos[0], part, h);
             eigenvalue = positive - negative;
-        elif self.type == '1b':
-            part = self.h / 2;
-            negative = intImg.getAreaSum(self.pos[1], self.pos[0], self.w, part);
-            positive = intImg.getAreaSum(self.pos[1], self.pos[0] + part, self.w, part);
+        elif ftype == '1b':
+            part = h / 2;
+            negative = intImg.getAreaSum(pos[1], pos[0], w, part);
+            positive = intImg.getAreaSum(pos[1], pos[0] + part, w, part);
             eigenvalue = positive - negative;
-        elif self.type == '1c':
-            part = self.w / 2;
-            negative = intImg.getAreaSum(self.pos[1] + self.h, self.pos[0], part, self.h, 45);
-            positive = intImg.getAreaSum(self.pos[1] + self.h + part, self.pos[0] + part, part, self.h, 45);
+        elif ftype == '1c':
+            part = w / 2;
+            negative = intImg.getAreaSum(pos[1] + h, pos[0], part, h, 45);
+            positive = intImg.getAreaSum(pos[1] + h + part, pos[0] + part, part, h, 45);
             eigenvalue = positive - negative;
-        elif self.type == '1d':
-            part = self.h / 2;
-            negative = intImg.getAreaSum(self.pos[1] + self.h, self.pos[0], self.w, part, 45);
-            positive = intImg.getAreaSum(self.pos[1] + part, self.pos[0] + part, self.w, part, 45);
+        elif ftype == '1d':
+            part = h / 2;
+            negative = intImg.getAreaSum(pos[1] + h, pos[0], w, part, 45);
+            positive = intImg.getAreaSum(pos[1] + part, pos[0] + part, w, part, 45);
             eigenvalue = positive - negative;
-        elif self.type == '2a':
-            part = self.w / 3;
-            negative1 = intImg.getAreaSum(self.pos[1], self.pos[0], part, self.h);
-            positive = intImg.getAreaSum(self.pos[1] + part, self.pos[0], part, self.h);
-            negative2 = intImg.getAreaSum(self.pos[1] + 2 * part, self.pos[0], part, self.h);
+        elif ftype == '2a':
+            part = w / 3;
+            negative1 = intImg.getAreaSum(pos[1], pos[0], part, h);
+            positive = intImg.getAreaSum(pos[1] + part, pos[0], part, h);
+            negative2 = intImg.getAreaSum(pos[1] + 2 * part, pos[0], part, h);
             eigenvalue = positive - negative1 - negative2;
-        elif self.type == '2b':
-            part = self.w / 4;
-            negative1 = intImg.getAreaSum(self.pos[1], self.pos[0], part, self.h);
-            positive = intImg.getAreaSum(self.pos[1] + part, self.pos[0], 2 * part, self.h);
-            negative2 = intImg.getAreaSum(self.pos[1] + 3 * part, self.pos[0], part, self.h);
+        elif ftype == '2b':
+            part = w / 4;
+            negative1 = intImg.getAreaSum(pos[1], pos[0], part, h);
+            positive = intImg.getAreaSum(pos[1] + part, pos[0], 2 * part, h);
+            negative2 = intImg.getAreaSum(pos[1] + 3 * part, pos[0], part, h);
             eigenvalue = positive - negative1 - negative2;
-        elif self.type == '2c':
-            part = self.h / 3;
-            negative1 = intImg.getAreaSum(self.pos[1], self.pos[0], self.w, part);
-            positive = intImg.getAreaSum(self.pos[1], self.pos[0] + part, self.w, part);
-            negative2 = intImg.getAreaSum(self.pos[1], self.pos[0] + 2 * part, self.w, part);
+        elif ftype == '2c':
+            part = h / 3;
+            negative1 = intImg.getAreaSum(pos[1], pos[0], w, part);
+            positive = intImg.getAreaSum(pos[1], pos[0] + part, w, part);
+            negative2 = intImg.getAreaSum(pos[1], pos[0] + 2 * part, w, part);
             eigenvalue = positive - negative1 - negative2;
-        elif self.type == '2d':
-            part = self.h / 4;
-            negative1 = intImg.getAreaSum(self.pos[1], self.pos[0], self.w, part);
-            positive = intImg.getAreaSum(self.pos[1], self.pos[0] + part, self.w, 2 * part);
-            negative2 = intImg.getAreaSum(self.pos[1], self.pos[0] + 3 * part, self.w, part);
+        elif ftype == '2d':
+            part = h / 4;
+            negative1 = intImg.getAreaSum(pos[1], pos[0], w, part);
+            positive = intImg.getAreaSum(pos[1], pos[0] + part, w, 2 * part);
+            negative2 = intImg.getAreaSum(pos[1], pos[0] + 3 * part, w, part);
             eigenvalue = positive - negative1 - negative2;
-        elif self.type == '2e':
-            part = self.w / 3;
-            negative1 = intImg.getAreaSum(self.pos[1] + self.h, self.pos[0], part, self.h, 45);
-            positive = intImg.getAreaSum(self.pos[1] + self.h + part, self.pos[0] + part, part, self.h, 45);
-            negative2 = intImg.getAreaSum(self.pos[1] + self.h + 2 * part, self.pos[0] + 2 * part, part, self.h, 45);
+        elif ftype == '2e':
+            part = w / 3;
+            negative1 = intImg.getAreaSum(pos[1] + h, pos[0], part, h, 45);
+            positive = intImg.getAreaSum(pos[1] + h + part, pos[0] + part, part, h, 45);
+            negative2 = intImg.getAreaSum(pos[1] + h + 2 * part, pos[0] + 2 * part, part, h, 45);
             eigenvalue = positive - negative1 - negative2;
-        elif self.type == '2f':
-            part = self.w / 4;
-            negative1 = intImg.getAreaSum(self.pos[1] + self.h, self.pos[0], part, self.h, 45);
-            positive = intImg.getAreaSum(self.pos[1] + self.h + part, self.pos[0] + part, 2 * part, self.h, 45);
-            negative2 = intImg.getAreaSum(self.pos[1] + self.h + 3 * part, self.pos[0] + 3 * part, part, self.h, 45);
+        elif ftype == '2f':
+            part = w / 4;
+            negative1 = intImg.getAreaSum(pos[1] + h, pos[0], part, h, 45);
+            positive = intImg.getAreaSum(pos[1] + h + part, pos[0] + part, 2 * part, h, 45);
+            negative2 = intImg.getAreaSum(pos[1] + h + 3 * part, pos[0] + 3 * part, part, h, 45);
             eigenvalue = positive - negative1 - negative2;
-        elif self.type == '2g':
-            part = self.h / 3;
-            negative1 = intImg.getAreaSum(self.pos[1] + self.h, self.pos[0], self.w, part, 45);
-            positive = intImg.getAreaSum(self.pos[1] + 2 * part, self.pos[0] + part, self.w, part, 45);
-            negative2 = intImg.getAreaSum(self.pos[1] + part, self.pos[0] + 2 * part, self.w, part, 45);
+        elif ftype == '2g':
+            part = h / 3;
+            negative1 = intImg.getAreaSum(pos[1] + h, pos[0], w, part, 45);
+            positive = intImg.getAreaSum(pos[1] + 2 * part, pos[0] + part, w, part, 45);
+            negative2 = intImg.getAreaSum(pos[1] + part, pos[0] + 2 * part, w, part, 45);
             eigenvalue = positive - negative1 - negative2;
-        elif self.type == '2h':
-            part = self.h / 4;
-            negative1 = intImg.getAreaSum(self.pos[1] + self.h, self.pos[0], self.w, part, 45);
-            positive = intImg.getAreaSum(self.pos[1] + 3 * part, self.pos[0] + part, self.w, 2 * part, 45);
-            negative2 = intImg.getAreaSum(self.pos[1] + part, self.pos[0] + 3 * part, self.w, part, 45);
+        elif ftype == '2h':
+            part = h / 4;
+            negative1 = intImg.getAreaSum(pos[1] + h, pos[0], w, part, 45);
+            positive = intImg.getAreaSum(pos[1] + 3 * part, pos[0] + part, w, 2 * part, 45);
+            negative2 = intImg.getAreaSum(pos[1] + part, pos[0] + 3 * part, w, part, 45);
             eigenvalue = positive - negative1 - negative2;
-        elif self.type == '3a':
-            partw = self.w / 3;
-            parth = self.h / 3;
-            whole = intImg.getAreaSum(self.pos[1], self.pos[0], self.w, self.h);
-            positive = intImg.getAreaSum(self.pos[1] + partw, self.pos[0] + parth, partw, parth);
+        elif ftype == '3a':
+            partw = w / 3;
+            parth = h / 3;
+            whole = intImg.getAreaSum(pos[1], pos[0], w, h);
+            positive = intImg.getAreaSum(pos[1] + partw, pos[0] + parth, partw, parth);
             eigenvalue = 2 * positive - whole;
-        elif self.type == '3b':
-            partw = self.w / 3;
-            parth = self.h / 3;
-            whole = intImg.getAreaSum(self.pos[1] + self.h, self.pos[0], self.w, self.h, 45);
-            positive = intImg.getAreaSum(self.pos[1] + partw + 2 * parth, self.pos[0] + partw + parth, partw, parth, 45);
+        elif ftype == '3b':
+            partw = w / 3;
+            parth = h / 3;
+            whole = intImg.getAreaSum(pos[1] + h, pos[0], w, h, 45);
+            positive = intImg.getAreaSum(pos[1] + partw + 2 * parth, pos[0] + partw + parth, partw, parth, 45);
             eigenvalue = 2 * positive - whole;
         
-        return eigenvalue;
+        return eigenvalue / (w * h);
     
     # 投票
-    def getVote(self, intImg):
-        score = self.getEigenvalue(intImg);
+    def getVote(self, wstep, hstep, multiple, intImg):
+        pos = (self.pos[0] + hstep, self.pos[1] + wstep);
+        w = self.w * multiple;
+        h = self.h * multiple;
+        score = self.getEigenvalue(self.type, pos, w, h, intImg);
+        score = score * multiple;
         if self.p * score <= self.p * self.theta:
             return 1;
         else:
@@ -268,35 +273,119 @@ def predict(images, model):
     for item in model:
         zw += item.weight;
     
+    posNum = 0;
+    negNum = 0;
+    tpr = 0;
+    fpr = 0;
     err = 0;
+    
     m = len(images);
     for i in range(m):
         print 'predicting img: ' + str(i + 1) + '/' + str(m);
         score = 0.0;
         pred = None;
         for item in model:
-            score += item.getVote(images[i]) * item.weight;
+            score += item.getVote(0, 0, 1, images[i]) * item.weight;
         
-        if score >= 0.6 * zw:
+        if score >= 0.5 * zw:
             pred = 1;
         else:
             pred = 0;
         
-        if pred != images[i].label:
-            err += 1;
+        if images[i].label == 1:
+            posNum += 1;
+            if pred == 1:
+                tpr += 1;
+            else:
+                err += 1;
+        else :
+            negNum += 1;
+            if pred == 1:
+                fpr += 1;
+                err += 1;
     
-    print 'Predict Accuracy: ' + str(float(m - err) * 100 / m) + '%';
+    print tpr, fpr;
+    tpr = float(tpr) * 100 / posNum;
+    fpr = float(fpr) * 100 / negNum;
+    print 'Accuracy: ' + str(float(m - err) * 100 / m) + '%, TPR: ' + str(tpr) + '%, FPR: ' + str(fpr) + '%';
 
-def findFace(image, model):
-    pass;
+# 查找人脸
+def findFace(image, model, detSize):
+    faces = {};
+    zw = 0.0;
+    for item in model:
+        zw += item.weight;
+    ih, iw = np.shape(image.orig);
+    
+    multiple = 1;
+    step = 6;
+    
+    while(detSize < iw or detSize < ih):
+        xstep = iw - (detSize + step) + 1;
+        ystep = ih - (detSize + step) + 1;
+        total = ((xstep / step) + 1) * ((ystep / step) + 1);
+        current = 0;
+        for i in range(0, ystep, step):
+            for j in range(0, xstep, step):
+                current += 1;
+                print 'detSize: ' + str(detSize) + ', inner: ' + str(current) + '/' + str(total);
+                score = 0.0;
+                for item in model:
+                    score += item.getVote(j, i, multiple, image) * item.weight;
+                
+                if score >= 0.5 * zw:
+                    if faces.has_key(multiple):
+                        tmp = faces.get(multiple);
+                        tmp.append([i, j]);
+                        faces[multiple] = tmp;
+                    else:
+                        faces[multiple] = [[i, j]];
+                    
+        multiple = multiple * 2;
+        detSize = detSize * 2;
+    
+    return faces;
 
+# 合并查找人脸的结果
+def combineFaces(faces, detSize):
+    T = 5;
+    combineResult = {};
+    for key in faces:
+        all = faces.get(key);
+        result = [];
+        if len(all) < T:
+            continue;
+        
+        R = (key * detSize) * (key * detSize);
+        while(len(all) > 0):
+            point = all[0];
+            stack = [];
+            all.remove(point);
+            for item in all:
+                if (np.square(point[0]-item[0]) + np.square(point[1]-item[1])) < R:
+                    stack.append(item);
+            if len(stack) > T:
+                size = len(stack) + 1;
+                target = point;
+                for item in stack:
+                    target = [target[0] + item[0], target[1] + item[1]];
+                    all.remove(item);
+                target = [target[0] / size, target[1] / size];
+                result.append(target);
+        
+        combineResult[key] = result;
+    
+    return combineResult;
+    
 def main():
     DEBUG = False;
-    modelfile = '/home/hadoop/ProgramDatas/MLStudy/FaceDection/adaboost_model.txt';
+    detSize = 24;
+#     modelfile = '/home/hadoop/ProgramDatas/MLStudy/FaceDection/adaboost_model.txt';
+    modelfile = 'E:/TestDatas/MLStudy/FaceDection/adaboost_model.txt';
     model = importAdaboostModel(modelfile);
     
 #     # check accuracy
-#     mitSrc = '/home/hadoop/ProgramDatas/MLStudy/FaceDection/MIT/';
+#     mitSrc = 'E:/TestDatas/MLStudy/FaceDection/MIT/';
 #     yaleSrc = '/home/hadoop/ProgramDatas/MLStudy/FaceDection/Yale/';
 #     orlSrc = '/home/hadoop/ProgramDatas/MLStudy/FaceDection/ORL/';
 #     feretSrc = '/home/hadoop/ProgramDatas/MLStudy/FaceDection/FERET/FERET_80_80/';
@@ -307,8 +396,23 @@ def main():
 #     feretDatas = loadFERET(feretSrc, 24, DEBUG);
 #     predict(mitDatas, model);
 
-    picFile = '/home/hadoop/ProgramDatas/MLStudy/FaceDection/LFW/lfw/Aaron_Eckhart/Aaron_Eckhart_0001.jpg';
+    picFile = 'z:/ViolaJones/lfw1.jpg';
     image = IntegralImage(readImage(picFile), -1);
-    findFace(image, model);
+#     plotDatas(image.orig);
+    faces = findFace(image, model, detSize);
+
+    faces = combineFaces(faces, detSize);
+    
+    img = Image.open(picFile);
+    img_d = ImageDraw.Draw(img);
+    
+    for mul in faces:
+        targets = faces[mul];
+        for face in targets:
+            distance = mul * detSize;
+            img_d.line(((face[0], face[1]), (face[0], face[1] + distance),
+                     (face[0] + distance, face[1] + distance),
+                    (face[0] + distance, face[1]), (face[0], face[1])), fill=150);
+        img.save('z:/ViolaJones/result.png');
 
 main();
